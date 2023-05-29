4f6de4b8cb55488cad2635087d0a78ed0b4feb43
jungju <leejungju.go@gmail.com>
2023-05-30 02:06:46 +0900

    Update

--- scripts/toggle_button.gd
+++ scripts/toggle_button.gd
@@ -8,6 +8,8 @@ func _process(_delta):
 	$off_icon.visible = button_pressed
 	$on_icon.visible = !button_pressed
 
+# button_pressed는 이미 있는 변수라 Warn 발생. 하지만 사용가능
+# 매개변수 없으면 동작안함. 버그
 func _on_toggled(button_pressed:bool):
 	GlobalMusicManager.play_select_sound()
 	Global.set_mute_audio(button_pressed)
